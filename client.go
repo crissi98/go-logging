e76aae94d1e7af5f04d73b76df9e6dec3521df2c
crissi98 <christian@radow.xyz>
2021-05-27 18:43:21 +0200

    added http client that automatically sets the correlation id from context

---
+++ client.go
@@ -0,0 +1,15 @@
+package logging
+
+import "net/http"
+
+type HttpClient struct {
+	client *http.Client
+}
+
+func (c *HttpClient) Do(r *http.Request) (*http.Response, error) {
+	correlationID := r.Context().Value(CorrelationIdContextKey)
+	if correlationID != nil {
+		r.Header.Add(CorrelationIdHeaderName, correlationID.(string))
+	}
+	return c.client.Do(r)
+}
